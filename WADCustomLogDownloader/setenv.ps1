1b9d858514bb65f4abe6639e020e82f6fa159c2a
Takekazu Omi <takekazuomi@gmail.com>
2015-09-26 11:33:39 +0900

    Update setenv.ps1

--- WADCustomLogDownloader/setenv.ps1
+++ WADCustomLogDownloader/setenv.ps1
@@ -4,5 +4,5 @@ Param(
 )
 
 Select-AzureSubscription -SubscriptionName $SubscriontName
-$storageKey = Get-AzureStorageKey -StorageAccountName kinmuginl01
+$storageKey = Get-AzureStorageKey -StorageAccountName $StorageAccountName
 $env:AZURE_STORAGE_CONNECTION_STRING=("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}" -f $StorageAccountName, $storageKey.Primary)
